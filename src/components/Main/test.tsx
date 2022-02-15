c5cf2e50571db876b086553974c1c36253c6d802
Pablo Santos <pablo.1999@alunos.utfpr.edu.br>
2022-02-15 18:51:04 -0300

    fix: editor config conflict

--- src/components/Main/test.tsx
+++ src/components/Main/test.tsx
@@ -7,7 +7,9 @@ describe('<Main />', () => {
     const { container } = render(<Main />)
 
     expect(
-      screen.getByRole('heading', { name: /react avançado/i })
+      screen.getByRole('heading', {
+        name: /react avançado/i
+      })
     ).toBeInTheDocument()
 
     expect(container.firstChild).toMatchSnapshot()
