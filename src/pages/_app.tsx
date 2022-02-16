c84bd6cdaa1b14a97b1589661cb8b73040d093a3
Pablo Santos <pablo.1999@alunos.utfpr.edu.br>
2022-02-16 11:41:45 -0300

    chore: add leaflet and react-leaflet for maps

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,6 +7,12 @@ function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
+        <link
+          rel="stylesheet"
+          href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css"
+          integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A=="
+          crossOrigin=""
+        />
         <title>My trips</title>
       </Head>
       <GlobalStyles />
