5b2e76d94d8bd922ba27ccf4e9128b2d189aae12
Pablo Santos <pablo.1999@alunos.utfpr.edu.br>
2022-02-28 08:50:51 -0300

    feat: add dinamic pins to places

--- src/graphql/queries.ts
+++ src/graphql/queries.ts
@@ -25,3 +25,25 @@ export const GET_PAGE_BY_SLUG = gql`
     }
   }
 `
+
+export const GET_PLACES = gql`
+  query getPlaces {
+    places {
+      id
+      location {
+        latitude
+        longitude
+      }
+      description {
+        html
+      }
+      name
+      slug
+      gallery {
+        url
+        height
+        width
+      }
+    }
+  }
+`
